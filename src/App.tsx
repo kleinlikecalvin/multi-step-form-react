4cc1270cde40998b269715c1908dc1d5c5f2a515
streetlightkids <ltj_hq@hotmail.com>
2023-12-18 17:01:44 -0700

    Reorganizes App

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,48 @@
 import React from "react";
 import "./App.scss";
-import StepOne from "./StepOne";
+// import StepOne from "./StepOne";
 import StepTwo from "./StepTwo";
-import StepThree from "./StepThree";
-import StepFour from "./StepFour";
-import StepFive from "./StepFive";
+// import StepThree from "./StepThree";
+// import StepFour from "./StepFour";
+// import StepFive from "./StepFive";
 
-function App() {
+export default function App() {
+  const cart = React.useState<{
+    plan: { name: string; monthlyCost: number; annualCost: number };
+    addOns?: { name: string; monthlyCost: number; annualCost: number }[];
+  }>({ plan: { name: "", monthlyCost: 0, annualCost: 0 }, addOns: [] });
+  const plans = [
+    { name: "arcade", monthlyCost: 9, annualCost: 90 },
+    { name: "advanced", monthlyCost: 12, annualCost: 120 },
+    { name: "pro", monthlyCost: 15, annualCost: 150 },
+  ];
+  const addOns = [
+    { name: "online service", monthlyCost: 1, annualCost: 10 },
+    { name: "larger storage", monthlyCost: 2, annualCost: 20 },
+    { name: "customizable profile", monthlyCost: 2, annualCost: 20 },
+  ];
   return (
     <div className="App">
       <ol className="steps">
-        <li>
-          <div className="step-info">
-            <p className="step">step 1</p>
-            <p className="step-details">your info</p>
-          </div>
-        </li>
-        <li>
-          <div className="step-info">
-            <p className="step">step 2</p>
-            <p className="step-details">select plan</p>
-          </div>
-        </li>
-        <li>
-          <div className="step-info">
-            <p className="step">step 3</p>
-            <p className="step-details">add-ons</p>
-          </div>
-        </li>
-        <li>
-          <div className="step-info">
-            <p className="step">step 4</p>
-            <p className="step-details">summary</p>
-          </div>
-        </li>
+        <WizardStepInfo name="step 1" details="your info" />
+        <WizardStepInfo name="step 2" details="select plan" />
+        <WizardStepInfo name="step 3" details="add-ons" />
+        <WizardStepInfo name="step 4" details="summary" />
       </ol>
-      <StepOne />
+      {/* <StepOne /> */}
       <StepTwo />
-      <StepThree />
-      <StepFour />
-      <StepFive />
+      {/* <StepThree />
+      <StepFour /> */}
+      {/* <StepFive /> */}
     </div>
   );
 }
 
-export default App;
+function WizardStepInfo({ name, details }: { name: string; details: string }) {
+  return (
+    <li className="WizardStepInfo">
+      <p className="step">{name}</p>
+      <p className="step-details">{details}</p>
+    </li>
+  );
+}
