59d4a1033a93bc12fa3b432971e15180213b0b22
streetlightkids <ltj_hq@hotmail.com>
2023-12-20 18:39:04 -0700

    Renamed files

---
+++ src/OrderConfirmation.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+export default function OrderConfirmation() {
+  return (
+    <div className="OrderConfirmation">
+      <img src="" alt="" />
+      <h1>Thank you!</h1>
+      <p>
+        Thanks for confirming your subscription! We hope you hae fun using our
+        platform. If you ever need support, please feel free to email us at
+        support@loregaming.com
+      </p>
+    </div>
+  );
+}
