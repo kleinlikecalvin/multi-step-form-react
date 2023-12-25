5979061800f41929bc205ad60bbf196765a63e9b
streetlightkids <ltj_hq@hotmail.com>
2023-12-25 13:21:02 -0700

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,23 @@
+import arcadeIcon from "./assets/images/icon-arcade.svg";
+import advancedIcon from "./assets/images/icon-advanced.svg";
+import proIcon from "./assets/images/icon-pro.svg";
+
+export const initialCart = { plan: { name: "" }, addOns: [] };
+
 export const plans = [
-  { name: "arcade", monthlyCost: 9, annualCost: 90 },
-  { name: "advanced", monthlyCost: 12, annualCost: 120 },
-  { name: "pro", monthlyCost: 15, annualCost: 150 },
+  {
+    name: "arcade",
+    icon: arcadeIcon,
+    monthlyCost: 9,
+    annualCost: 90,
+  },
+  {
+    name: "advanced",
+    icon: advancedIcon,
+    monthlyCost: 12,
+    annualCost: 120,
+  },
+  { name: "pro", icon: proIcon, monthlyCost: 15, annualCost: 150 },
 ] as const;
 
 export const addOns = [
