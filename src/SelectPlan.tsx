283bf0c1d16c530999369960bda858af262e82c1
streetlightkids <ltj_hq@hotmail.com>
2023-12-25 13:21:26 -0700

    Adds interactivity to SelectPlan

--- src/SelectPlan.tsx
+++ src/SelectPlan.tsx
@@ -1,80 +1,83 @@
 import React from "react";
-import { plans } from "./constants";
+import { plans, initialCart } from "./constants";
+import StepHeader from "./StepHeader";
 import NavButtons from "./NavButtons";
 
 // type Unarray<T> = T extends Array<infer U> ? U : T;
 // type Plan = Unarray<typeof plans>['']
 
-/**
- * clean up CSS
- * componentize (dumb component) the h1 & p because it's used in every step
- * Add icons to the plans
- * Conditional logic for plan selected css, for monthly/yearly css
- */
-
-export default function SelectPlan() {
-  // Set a default plan to be selected
-  const [planSelected, setPlanSelected] = React.useState("");
+export default function SelectPlan({
+  cart,
+  updateCart,
+  updateActive,
+}: {
+  cart: {
+    plan: { name: string };
+    addOns?: { name: string; monthlyCost: number; annualCost: number }[];
+  };
+  updateCart: Function;
+  updateActive: Function;
+}) {
+  const [planSelected, setPlanSelected] = React.useState("advanced");
   const [subscriptionCycle, setSubscriptionCycle] = React.useState<
     "monthly" | "yearly"
   >("yearly");
-  const monthly = subscriptionCycle === "monthly";
+  const isMonthly = subscriptionCycle === "monthly";
+  const discount = "2 months free";
 
   return (
-    <form className="SelectPlan">
-      <h1>Select your plan</h1>
-      <p>You have the option of monthly or yearly billing.</p>
+    <form className="SelectPlan wizard-step">
+      <StepHeader
+        title="Select your plan"
+        desc="You have the option of monthly or yearly billing."
+      />
       <div className="plans">
         {plans.map((plan) => (
-          <Plan
-            icon=""
-            planName={plan.name}
-            price={monthly ? plan.monthlyCost : plan.annualCost}
-            discountLine={monthly ? undefined : "2 months free"}
-          />
+          <button
+            className={plan.name === planSelected ? "plan active" : "plan"}
+            onClick={(e) => {
+              e.preventDefault();
+              setPlanSelected(plan.name);
+            }}
+          >
+            <img src={plan.icon} alt="" />
+            <div>
+              <strong className="name">{plan.name}</strong>
+              <small className="price">
+                ${isMonthly ? plan.monthlyCost : plan.annualCost}/
+                {isMonthly ? "mo" : "yr"}
+              </small>
+              {!isMonthly && <small>{discount}</small>}
+            </div>
+          </button>
         ))}
       </div>
       <div className="toggle-container">
-        <strong>monthly</strong>
+        <strong className={isMonthly ? "active" : undefined}>monthly</strong>
         <label className="toggle">
           <input
             type="checkbox"
-            checked={!monthly}
+            checked={!isMonthly}
             onChange={(e) =>
               setSubscriptionCycle(e.target.checked ? "yearly" : "monthly")
             }
           />
           <span className="slider"></span>
         </label>
-        <strong>yearly</strong>
+        <strong className={!isMonthly ? "active" : undefined}>yearly</strong>
       </div>
-      {/* 
-      (onclick)
-      Need to pass a callback to next where we update cart state
-      Need to pass a callback to go back to reset all info of either cart and this step or just this step. Think about that more.
-      */}
-      <NavButtons />
+      <NavButtons
+        backClick={(e) => {
+          e.preventDefault();
+          updateCart(initialCart);
+          updateActive(1);
+        }}
+        nextClick={(e) => {
+          e.preventDefault();
+          updateCart({ ...cart, plan: { name: planSelected } });
+          updateActive(3);
+        }}
+      />
     </form>
-  );
-}
-
-function Plan({
-  icon,
-  planName,
-  price,
-  discountLine,
-}: {
-  icon: string;
-  planName: string;
-  price: number;
-  discountLine?: string;
-}) {
-  return (
-    <button className="Plan">
-      <img src={icon} alt="" />
-      <strong>{planName}</strong>
-      <small>${price}</small>
-      {discountLine}
-    </button>
   );
 }
