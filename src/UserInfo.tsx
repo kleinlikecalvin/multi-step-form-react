a759a4c8bcbe5056548d8b2216e169c5ee60a586
streetlightkids <ltj_hq@hotmail.com>
2023-12-25 13:20:53 -0700

    Update UserInfo.tsx

--- src/UserInfo.tsx
+++ src/UserInfo.tsx
@@ -1,15 +1,38 @@
 import React from "react";
-export default function UserInfo() {
+import StepHeader from "./StepHeader";
+import NavButtons from "./NavButtons";
+export default function UserInfo({
+  cart,
+  updateCart,
+  updateActive,
+}: {
+  cart: {
+    plan: { name: string };
+    addOns?: { name: string; monthlyCost: number; annualCost: number }[];
+  };
+  updateCart: Function;
+  updateActive: Function;
+}) {
   return (
     <form>
-      <h1>Personal Info</h1>
-      <p>Please provide your name, email address, and phone number.</p>
+      <StepHeader
+        title="Personal Info"
+        desc="Please provide your name, email address, and phone number."
+      />
       <label htmlFor="name">Name</label>
       <input type="text" placeholder="e.g. Stephen King" name="name" />
       <label htmlFor="email">Email Address</label>
       <input type="email" name="email" />
       <label htmlFor="phone">Phone Number</label>
       <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="phone" />
+      <NavButtons
+        backClick={(e) => {
+          e.preventDefault();
+        }}
+        nextClick={(e) => {
+          e.preventDefault();
+        }}
+      />
     </form>
   );
 }
