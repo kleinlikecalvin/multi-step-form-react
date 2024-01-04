69284aa9f44ad28486895aeb4542dcbb752c752f
streetlightkids <ltj_hq@hotmail.com>
2024-01-03 18:27:04 -0700

    Update OrderConfirmation.tsx

--- src/OrderConfirmation.tsx
+++ src/OrderConfirmation.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import thankYou from "./assets/images/icon-thank-you.svg";
 export default function OrderConfirmation() {
   return (
     <div className="OrderConfirmation">
-      <img src="" alt="" />
+      <img src={thankYou} alt="" />
       <h1>Thank you!</h1>
       <p>
         Thanks for confirming your subscription! We hope you hae fun using our
