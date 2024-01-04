f5439dedbed23935dae0ee445b2d7ac8b6c8449b
streetlightkids <ltj_hq@hotmail.com>
2024-01-03 18:26:45 -0700

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -2,26 +2,50 @@ import arcadeIcon from "./assets/images/icon-arcade.svg";
 import advancedIcon from "./assets/images/icon-advanced.svg";
 import proIcon from "./assets/images/icon-pro.svg";
 
-export const initialCart = { plan: { name: "" }, addOns: [] };
+export type Cart = {
+  plan: keyof typeof plans;
+  subscriptionCycle: string;
+  addOns?: (keyof typeof addOns)[];
+};
 
-export const plans = [
-  {
-    name: "arcade",
+export const initialCart: Cart = {
+  plan: "advanced",
+  subscriptionCycle: "monthly",
+  addOns: [],
+};
+
+export const plans = {
+  arcade: {
     icon: arcadeIcon,
     monthlyCost: 9,
     annualCost: 90,
   },
-  {
-    name: "advanced",
+  advanced: {
     icon: advancedIcon,
     monthlyCost: 12,
     annualCost: 120,
   },
-  { name: "pro", icon: proIcon, monthlyCost: 15, annualCost: 150 },
-] as const;
+  pro: {
+    icon: proIcon,
+    monthlyCost: 15,
+    annualCost: 150,
+  },
+} as const;
 
-export const addOns = [
-  { name: "online service", monthlyCost: 1, annualCost: 10 },
-  { name: "larger storage", monthlyCost: 2, annualCost: 20 },
-  { name: "customizable profile", monthlyCost: 2, annualCost: 20 },
-] as const;
+export const addOns = {
+  "online service": {
+    details: "Access to multiplayer games",
+    monthlyCost: 1,
+    annualCost: 10,
+  },
+  "larger storage": {
+    details: "Extra 1TB of cloud save",
+    monthlyCost: 2,
+    annualCost: 20,
+  },
+  "customizable profile": {
+    details: "Custom theme on your profile",
+    monthlyCost: 2,
+    annualCost: 20,
+  },
+} as const;
