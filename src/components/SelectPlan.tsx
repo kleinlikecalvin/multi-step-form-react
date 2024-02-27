d7ed961cbbf8bde5b5d3ceada977eb1a2945df5f
streetlightkids <ltj_hq@hotmail.com>
2024-02-27 13:31:02 -0700

    Updates to support mobile styling

--- src/components/SelectPlan.tsx
+++ src/components/SelectPlan.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { plans, initialCart, Cart } from "../constants";
+import { plans, Plans, Cart } from "../constants";
 import StepHeader from "./StepHeader";
 import NavButtons from "./NavButtons";
 import "./SelectPlan.scss";
@@ -13,7 +13,7 @@ export default function SelectPlan({
   updateCart: Function;
   updateActiveStep: Function;
 }) {
-  const [planSelected, setPlanSelected] = React.useState("advanced");
+  const [planSelected, setPlanSelected] = React.useState(cart.plan);
   const [subscriptionCycle, setSubscriptionCycle] = React.useState<
     "monthly" | "yearly"
   >(cart.subscriptionCycle);
@@ -21,49 +21,50 @@ export default function SelectPlan({
   const discount = "2 months free";
 
   return (
-    <div className="SelectPlan grid">
-      <StepHeader
-        title="Select your plan"
-        desc="You have the option of monthly or yearly billing."
-      />
-      <div className="plans">
-        {Object.entries(plans).map(([name, plan]) => (
-          <button
-            className={name === planSelected ? "plan active" : "plan"}
-            onClick={() => {
-              setPlanSelected(name);
-            }}
-          >
-            <img src={plan.icon} alt="" />
-            <div>
-              <strong className="name">{name}</strong>
-              <small className="price">
-                ${isMonthly ? plan.monthlyCost : plan.annualCost}/
-                {isMonthly ? "mo" : "yr"}
-              </small>
-              {!isMonthly && <small>{discount}</small>}
-            </div>
-          </button>
-        ))}
-      </div>
-      <div className="toggle-container">
-        <strong className={isMonthly ? "active" : undefined}>monthly</strong>
-        <label className="toggle">
-          <input
-            type="checkbox"
-            checked={!isMonthly}
-            onChange={(e) =>
-              setSubscriptionCycle(e.target.checked ? "yearly" : "monthly")
-            }
-          />
-          <span className="slider"></span>
-        </label>
-        <strong className={!isMonthly ? "active" : undefined}>yearly</strong>
+    <div className="SelectPlan grid-d">
+      <div className="content">
+        <StepHeader
+          title="Select your plan"
+          desc="You have the option of monthly or yearly billing."
+        />
+        <div className="plans">
+          {Object.entries(plans).map(([name, plan]) => (
+            <button
+              className={name === planSelected ? "plan active" : "plan"}
+              onClick={() => {
+                setPlanSelected(name as Plans);
+              }}
+            >
+              <img src={plan.icon} alt="" />
+              <div className="desc">
+                <strong className="name">{name}</strong>
+                <small className="price">
+                  ${isMonthly ? plan.monthlyCost : plan.annualCost}/
+                  {isMonthly ? "mo" : "yr"}
+                </small>
+                {!isMonthly && <small>{discount}</small>}
+              </div>
+            </button>
+          ))}
+        </div>
+        <div className="toggle-container">
+          <strong className={isMonthly ? "active" : undefined}>monthly</strong>
+          <label className="toggle">
+            <input
+              type="checkbox"
+              checked={!isMonthly}
+              onChange={(e) =>
+                setSubscriptionCycle(e.target.checked ? "yearly" : "monthly")
+              }
+            />
+            <span className="slider"></span>
+          </label>
+          <strong className={!isMonthly ? "active" : undefined}>yearly</strong>
+        </div>
       </div>
       <NavButtons
         hideButton={false}
         backClick={() => {
-          updateCart(initialCart);
           updateActiveStep(1);
         }}
         nextClick={() => {
