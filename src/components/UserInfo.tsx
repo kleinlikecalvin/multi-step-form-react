68c4d7ef2b780570785c54f7d172577388ba0df8
streetlightkids <ltj_hq@hotmail.com>
2024-02-27 13:30:49 -0700

    Updates to support mobile styling

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,46 +1,122 @@
+import React from "react";
+import { Cart } from "../constants";
 import StepHeader from "./StepHeader";
 import NavButtons from "./NavButtons";
 import "./UserInfo.scss";
 
+const FIELD_REQ_MESSAGE = "This field is required";
+const EMAIL_INVALID = "Email is invalid";
+const PHONE_INVALID = "Phone number is invalid";
+
 export default function UserInfo({
+  cart,
+  updateCart,
   updateActiveStep,
 }: {
+  cart: Cart;
+  updateCart: Function;
   updateActiveStep: Function;
 }) {
+  const [name, setName] = React.useState(cart.userInfo.name);
+  const [nameError, setNameError] = React.useState("");
+  const [email, setEmail] = React.useState(cart.userInfo.email);
+  const [emailError, setEmailError] = React.useState("");
+  const [phone, setPhone] = React.useState(cart.userInfo.phone);
+  const [phoneError, setPhoneError] = React.useState("");
+
   return (
-    <div className="UserInfo grid">
-      <StepHeader
-        title="Personal Info"
-        desc="Please provide your name, email address, and phone number."
-      />
-      <form>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          placeholder="e.g. Stephen King"
-          name="name"
-          required
+    <div className="UserInfo grid-d">
+      <section className="content">
+        <StepHeader
+          title="Personal Info"
+          desc="Please provide your name, email address, and phone number."
         />
-        <label htmlFor="email">Email Address</label>
-        <input
-          type="email"
-          name="email"
-          placeholder="e.g. email@website.com"
-          required
-        />
-        <label htmlFor="phone">Phone Number</label>
-        <input
-          type="tel"
-          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-          name="phone"
-          placeholder="e.g. +1 234 567 8910"
-          required
-        />
-      </form>
+        <form>
+          <label htmlFor="name">
+            Name
+            {nameError && <span className="error active">{nameError}</span>}
+          </label>
+          <input
+            type="text"
+            placeholder="e.g. Stephen King"
+            name="name"
+            value={name}
+            className={nameError ? "info error active" : "info"}
+            onChange={(e) => {
+              setNameError("");
+              setName(e.target.value);
+            }}
+          />
+          <label htmlFor="email">
+            Email Address
+            {emailError && <span className="error active">{emailError}</span>}
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="e.g. email@website.com"
+            value={email}
+            className={emailError ? "info error active" : "info"}
+            onChange={(e) => {
+              setEmailError("");
+              setEmail(e.target.value);
+            }}
+          />
+          <label htmlFor="phone">
+            Phone Number
+            {phoneError && <span className="error active">{phoneError}</span>}
+          </label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="e.g. +1 234 567 8910"
+            value={phone}
+            className={phoneError ? "info error active" : "info"}
+            onChange={(e) => {
+              setPhoneError("");
+              setPhone(e.target.value);
+            }}
+          />
+        </form>
+      </section>
       <NavButtons
         hideButton={true}
         nextClick={() => {
-          updateActiveStep(2);
+          const emailRegExp = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/;
+          const phoneRegExp =
+            /^(1\s?)?(\d{3}|\(\d{3}\))[\s\-]?\d{3}[\s\-]?\d{4}$/;
+          let errors = false;
+
+          if (name === "") {
+            setNameError(FIELD_REQ_MESSAGE);
+            errors = true;
+          }
+          if (email === "") {
+            setEmailError(FIELD_REQ_MESSAGE);
+            errors = true;
+          } else if (!emailRegExp.test(email)) {
+            setEmailError(EMAIL_INVALID);
+            errors = true;
+          }
+          if (phone === "") {
+            setPhoneError(FIELD_REQ_MESSAGE);
+            errors = true;
+          } else if (!phoneRegExp.test(phone)) {
+            setPhoneError(PHONE_INVALID);
+            errors = true;
+          }
+
+          if (!errors) {
+            updateCart((cart: Cart) => ({
+              ...cart,
+              userInfo: {
+                name,
+                email,
+                phone,
+              },
+            }));
+            updateActiveStep(2);
+          }
         }}
       />
     </div>
